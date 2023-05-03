92a412f9902bd96b71bf418a88e7fd5809e5d0a1
Avil Lobo <avil07lobo@gmail.com>
2023-05-02 17:20:45 -0700

    updates to slides, index.html and setup script file

--- setup.sh
+++ setup.sh
@@ -1,18 +1,24 @@
-#!/bin/sh
-echo "\n"
+#!/bin/bash
+echo -e "\n"
 echo "========================================================================================================"
 echo "Running script!!!"
 echo "========================================================================================================"
+
+#Add export command with variable name and value to .bash_profile
 echo 'export MYUSER="avil07lobo"' >> ~/.bash_profile
-echo "\n"
+echo -e "\n"
+
 echo "========================================================================================================"
 echo "Display the contents of .bash_profile with environment variable added!!!"
 echo "========================================================================================================"
 cat ~/.bash_profile
-echo "\n"
+echo -e "\n"
+
+#Apply changes to current session
 source ~/.bash_profile
+
 echo "========================================================================================================"
 echo "Display my environment variable!!"
 echo "========================================================================================================"
 echo $MYUSER
-echo "\n"
+echo -e "\n"
